847a1ad8c0e6597e6c134a5a25cc35c30fa3d446
Geoffroy Baumier <geoffroy.baumier@ufirstgroup.ch>
2023-11-28 12:55:06 +0100

    cleanup types

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+export declare const ignoreLeadingZeros: (input: string) => string;
+export declare const ignoreSpaces: (input: string) => string;
+export declare const ignoreNonNumericCharacters: (input: string) => string;
+export declare const ignoreNonAlphaNumericCharacters: (input: string) => string;
+export declare const ignoreAccentuatedCharacters: (input: string) => string;
+export declare const ignoreStreetVariations: (input: string) => string;
+export declare const sanitizePostalCode: (postalCode: string) => string;
+export declare const sanitizeHouseNumber: (houseNumber: string) => string;
+export declare const sanitizeStreetName: (streetName: string) => string;
--- lib/constants.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const SANITIZED_STREET_KEYWORD = "strasse";
-export declare const INVALID_STREET_KEYWORDS: string[];
-export declare const ACCENTUATED_CHARACTERS_MAPPING: Map<string, string>;
